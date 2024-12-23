c08e5dae5a0afc696baa24d3e207f14aac16a110
unknown <mafzal.sahi22@gmail.com>
2024-12-23 13:50:41 +0300

    Add initial versions of 05-password_generator

--- 05-password_generator/src/App.jsx
+++ 05-password_generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback,useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -12,14 +12,15 @@ function App() {
     let str="AbCdEfGhIjKlMnOpQrStUvWxYzAbCdEf"
     if(numberAllowed) str +="0123456789"
     if(charAllowed) str +="!@#$%^&*()_+[]{}|;:',.<>?/`~-="
-    for (let i = 0; i <= array.length; i++) {
+    for (let i = 0; i <= length; i++) {
       let char=Math.floor(Math.random()*str.length+1);
-      pass=str.charAt(char);
+      pass += str.charAt(char);
       
     }
     setPassword(pass)
 
   },[length,numberAllowed,charAllowed,setPassword])
+  useEffect(()=>{passwordGenerator()},[length,numberAllowed,charAllowed,passwordGenerator])
 
   return (
     <>
@@ -35,6 +36,35 @@ function App() {
         />
         <button
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flext text-center gap-x-1'>
+          <input
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className='flext text-center gap-x-1'>
+          <input 
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {setNumberAllowed((prev) => !prev)}}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+        </div>
+        <input 
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="charsInput"
+            onChange={() => {setCharAllowed((prev) => !prev)}}
+            />
+            <label htmlFor='charsInput'>Characters</label>
       </div>
     </div>
     </>
